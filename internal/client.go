77dfc136b52e16413215b53f6826542c78cc30ef
elmehdibelfkih <elmehdibelfkih@gmail.com>
2025-02-02 03:40:09 +0100

    stable version

--- internal/client.go
+++ internal/client.go
@@ -1,3 +1,105 @@
 package internal
 
-// import "net"
+import (
+	"io"
+	"net"
+	"os"
+	"time"
+	"unicode"
+)
+
+type Client struct {
+	noPrefix       bool
+	Authentication bool
+	Name           string
+	Message        string
+	conn           net.Conn
+	s              *Server
+	isReady        bool
+}
+
+func (cl *Client) handleInput() {
+	if cl.Name == "" {
+		if cl.Message == "\n" {
+			cl.sendMessage(EMPTY_MESSAGE_ERROR)
+			cl.conn.Write([]byte("[ENTER YOUR NAME]: "))
+		} else {
+			cl.Name = cl.Message[:len(cl.Message)-1]
+			if !cl.isValideName(cl.Name) {
+				cl.conn.Write([]byte("[ENTER YOUR NAME]: "))
+				return
+			}
+			cl.Message = ""
+			cl.Authentication = true
+			cl.noPrefix = true
+			cl.Message = cl.Name + " has joinned our chat...\n"
+			cl.s.broadcastMessage(cl.conn)
+			cl.noPrefix = false
+			data, err := os.ReadFile(LOGS_FILE_PATH)
+			if err == nil {
+				cl.sendMessage(string(data))
+			}
+		}
+	} else if cl.Message != "\n" {
+		cl.s.broadcastMessage(cl.conn)
+	} else {
+		cl.sendMessage(EMPTY_MESSAGE_ERROR)
+	}
+}
+
+func (cl *Client) sendMessage(message string) {
+	cl.conn.Write([]byte(message))
+}
+
+func (cl *Client) endConnection() {
+	cl.s.clientCounterMutex.Lock()
+	cl.s.clientCounter--
+	cl.s.clientCounterMutex.Unlock()
+	if cl.s.nextProtoErr == io.EOF {
+		cl.Message = cl.Name + " has left our chat...\n"
+	} else {
+		cl.Message = cl.Name + "error connection\n"
+	}
+	cl.noPrefix = true
+	cl.s.broadcastMessage(cl.conn)
+	cl.noPrefix = false
+	delete(cl.s.clients, cl.conn)
+}
+
+func (cl *Client) getPrefix() string {
+	currentTime := time.Now()
+	return "[" + currentTime.Format("2006-01-02 15:04:05") + "][" + cl.Name + "]:"
+}
+
+func (cl *Client) FormatedMessage() string {
+	if cl.Name == "" {
+		return cl.Message
+	}
+	return cl.getPrefix() + cl.Message
+}
+
+func (cl *Client) isValideName(name string) bool {
+	if len(cl.Name) > MAX_NAME_LEN {
+		cl.sendMessage(TOO_LONG_NAME)
+		cl.Name = ""
+		return false
+	}
+	for _, c := range name {
+		if !unicode.IsLetter(c) && c != ' ' && c != '-' && c != '\'' {
+			cl.sendMessage(INVALID_NAME)
+			cl.Name = ""
+			return false
+		}
+	}
+	return cl.isUniqueName()
+}
+func (cl *Client) isUniqueName() bool {
+	for cn, clt := range cl.s.clients {
+		if cn != cl.conn && clt.Name == cl.Name {
+			cl.sendMessage(DUPLICATE_NAME)
+			cl.Name = ""
+			return false
+		}
+	}
+	return true
+}
--- internal/server.go
+++ internal/server.go
@@ -1,61 +1,93 @@
 package internal
 
-import "net"
-import "sync"
-
-type message struct {
-	Time    string
-	Name    string
-	Message string
-}
-
-var Message message
+import (
+	"fmt"
+	"net"
+	"strings"
+	"sync"
+)
 
 type Server struct {
-	Addr string
-	// ReadTimeout     time.Duration
-	// MaxMessageBytes int // o
-	// ErrorLog        *log.Logger
-	// nextProtoOnce   sync.Once
-	// nextProtoErr    error
-	mu            sync.Mutex
-	listeners     net.Listener
-	listenerGroup sync.WaitGroup
-	conn          net.Conn
+	Addr               string
+	clientCounter      int
+	clientCounterMutex sync.Mutex
+	listener           net.Listener
+	clients            map[net.Conn]*Client
+	nextProtoErr       error
 }
 
 func NewServer(addr string) *Server {
-	server := Server{Addr: addr}
+	server := Server{Addr: addr, clients: make(map[net.Conn]*Client)}
 	return &server
-
 }
 
 func (s *Server) StartServer() error {
-	ln, err := net.Listen("tcp", s.Addr)
+	ln, err := net.Listen("tcp", ":"+s.Addr)
 	if err != nil {
 		return err
 	}
 	defer ln.Close()
-	s.listeners = ln
-	s.acceptNewConnection()
-	return nil
+	s.listener = ln
+	return s.acceptNewConnection()
 }
 
 func (s *Server) acceptNewConnection() error {
 	for {
-		conn, err := s.listeners.Accept()
-		s.conn = conn
-		if err != nil {
-			return err
+		conn, err := s.listener.Accept()
+		if s.clientCounter < 10 {
+			s.clientCounterMutex.Lock()
+			s.clientCounter++
+			s.clientCounterMutex.Unlock()
+			s.clients[conn] = &Client{s: s, conn: conn}
+			if err != nil {
+				return err
+			}
+			go s.handleConnection(conn)
+		} else if err == nil {
+			conn.Write([]byte(FULL_SERVER_ERROR))
+			conn.Close()
 		}
-		s.listenerGroup.Add(1)
-		go s.handleConnection()
 	}
 }
 
-func (s *Server) handleConnection() {
+func (s *Server) handleConnection(conn net.Conn) {
+	defer conn.Close()
 	var buf = make([]byte, 2048)
-	// conn, err := s.conn.Read(buf)
-	println(string(buf))
-	defer s.conn.Close()
+	conn.Write(LINUX_LOGO)
+	conn.Write([]byte("[ENTER YOUR NAME]: "))
+	for {
+		byteReaded, err := conn.Read(buf)
+		s.nextProtoErr = err
+		if err != nil {
+			s.clients[conn].endConnection()
+			return
+		} else { // FIXME: handel long input
+			s.clients[conn].Message += string(buf[:byteReaded])
+			if strings.Contains(s.clients[conn].Message, "\n") {
+				s.clients[conn].isReady = true
+			}
+		}
+		if s.clients[conn].isReady {
+			s.clients[conn].handleInput()
+			s.clients[conn].Message = ""
+			s.clients[conn].isReady = false
+		}
+	}
+}
+
+func (s *Server) broadcastMessage(conn net.Conn) {
+	if !s.clients[conn].noPrefix {
+		fmt.Fprint(LOGS_FILE, s.clients[conn].FormatedMessage())
+	}
+	for cn, cl := range s.clients {
+		if cn != conn {
+			if cl.Authentication {
+				if s.clients[conn].noPrefix {
+					cl.sendMessage(s.clients[conn].Message)
+				} else {
+					cl.sendMessage(s.clients[conn].FormatedMessage())
+				}
+			}
+		}
+	}
 }
